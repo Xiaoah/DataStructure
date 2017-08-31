0376dbf7ce433b8a36910b8f5706cf19752724cd
xiaoah <136638556@qq.com>
2017-08-31 20:12:25 +0800

    staticlink list

--- DataStructure/Linetable_list.cpp
+++ DataStructure/Linetable_list.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<iostream>
+#include<time.h>
 
 using namespace std;
 
@@ -17,6 +18,37 @@ typedef struct Node          //这里的Node不能省略
 
 typedef struct Node* LinkList;
 
+void CreateListHead(LinkList* L, int n)             //表头插入法创建一个链表
+{
+	LinkList p;
+	srand(time(0));
+	*L = (LinkList)malloc(sizeof(Node));
+	(*L)->next = NULL;
+	for (int i = 0; i < n; i++) {
+		p = (LinkList)malloc(sizeof(Node));
+		p->data = rand() % 100 + 1;
+		p->next = (*L)->next;
+		(*L)->next = p;
+	}
+
+}
+
+void CreateListTail(LinkList *L, int n)                //表尾插入法创建一个新表
+{
+	LinkList p, r;
+	srand(time(0));
+	*L = (LinkList)malloc(sizeof(Node));
+	r = *L;
+	for (int i = 0; i < n; i++)
+	{
+		p = (Node *)malloc(sizeof(Node));
+		p->data = rand() % 100 + 1;
+		r->next = p;
+		r = p;
+	}
+	r->next = NULL;
+}
+
 Status GetElem(LinkList L,int i,ElemType *e) {
 	LinkList p = L->next;
 	int j = 1;
@@ -32,19 +64,90 @@ Status GetElem(LinkList L,int i,ElemType *e) {
 	return OK;
 }
 
+Status ListInsert(LinkList* L, int i, ElemType e)
+{
+	LinkList p,s;
+	p = *L;
+	int j = 1;
+	while (p && j < i)                  //实际上寻找的是第i-1个结点
+	{
+		p = p->next;
+		j++;
+	}
+	if (!p || j>i) 
+	{
+		return ERROR;
+	}
+	s = (LinkList)malloc(sizeof(Node));
+	s->data = e;
+	s->next = p->next;
+	p->next = s;
+
+//	Node w;
+//	w.data = e;
+//	w.next = p->next;
+//	p->next = &w;
+
+	return OK;
+
+}
+
+Status ListDelete(LinkList *L, int i, ElemType *e)
+{
+	LinkList p, q;
+	p = *L;
+	int j = 1;
+	while (p->next && j < i)
+	{
+		p = p->next;
+		++j;
+	}
+	if (!(p->next) || j>i)
+	{
+		return ERROR;
+	}
+	q = p->next;
+	p->next = q->next;
+	*e = q->data;
+	free(q);
+	return OK;
+}
+
+Status ClearList(LinkList* L)
+{
+	LinkList p, q;
+	p = (*L)->next;
+	while (p)
+	{
+		q = p->next;
+		free(p);
+		p = q;
+	}
+	(*L)->next = NULL;
+	return OK;
+}
+/*
 int main(void)
 {
 	Node tail = { 3,NULL };
-	Node mid2 = { 2,&tail };
-	Node mid1 = { 1,&mid2 };
+	Node secondnode = { 2,&tail };
+	Node firstnode = { 1,&secondnode };
 	Node head;
-	head.next = &mid1;
+	head.next = &firstnode;
 	LinkList L = &head;
 
 	int elem = 0;
 	GetElem(L, 2, &elem);
 	printf("%d\n", elem);
-
+	printf("insert the 23 in the second node\n");
+	ListInsert(&L, 2, 23);
+	GetElem(L, 2, &elem);
+	printf("%d\n", elem);
+	ListDelete(&L, 2, &elem);
+	GetElem(L, 2, &elem);
+	printf("%d\n", elem);
+	
 
 	return 0;
 }
+*/
---
+++ DataStructure/StaticLinkList.cpp
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+typedef int ElemType;
+typedef int Status;
+
+#define OK 1
+#define ERROR 0
+#define MAXSIZE 1000
+
+typedef struct
+{
+	ElemType data;
+	int cur;
+}Component, StaticLinkList[MAXSIZE];
+
+Status InitList(StaticLinkList space)
+{
+	for (int i = 0; i < MAXSIZE-1; i++)
+	{
+		space[i].cur = i + 1;
+	}
+	space[MAXSIZE - 1].cur = 0;
+	return OK;
+}
+
+int Malloc_SLL(StaticLinkList space)            //返回备用空间链表的下标
+{
+	int i = space[0].cur;
+	if (space[0].cur)
+	{
+		space[0].cur = space[i].cur;
+	}
+	return i;
+}
+
+int ListLength(StaticLinkList space)
+{
+	int j = 0;
+	int i = space[MAXSIZE - 1].cur;
+	while (i)
+	{
+		i = space[i].cur;
+		j++;
+	}
+	return j;
+}
+
+Status ListInsert(StaticLinkList L, int i, ElemType e)
+{
+	int k = MAXSIZE - 1;
+	if (i<1 || i> ListLength(L) + 1) {
+		return ERROR;
+	}
+	int j = Malloc_SLL(L);
+	if (j)
+	{
+		L[j].data = e;
+		for (int l = 1; l <= i - 1; l++)
+		{
+			k = L[k].cur;
+		}
+		L[j].cur = L[k].cur;
+		L[k].cur = j;
+		return OK;
+	}
+	return ERROR;
+}
+
+void Free_SLL(StaticLinkList space, int k)        //回收删除的节点
+{
+	space[k].cur = space[0].cur;
+	space[0].cur = k;
+}
+
+Status ListDelete(StaticLinkList L, int i)
+{
+	if (i<1 || i>ListLength(L))
+	{
+		return ERROR;
+	}
+	int k = MAXSIZE - 1;
+	int j;
+	for (j = 1; j <= i - 1; j++)
+	{
+		k = L[k].cur;
+	}
+	j = L[k].cur;
+	L[k].cur = L[j].cur;
+	Free_SLL(L, j);
+	return OK;
+}
+
+int main(void)
+{
+	StaticLinkList space;
+	InitList(space);
+	//printf("%d\n", Malloc_SLL(space));
+	ListInsert(space, 1, 11);
+	ListInsert(space, 2, 22);
+	ListInsert(space, 3, 33);
+	printf("%d  %d\n", space[0].data,space[0].cur);
+	printf("%d  %d\n", space[1].data,space[1].cur);
+	printf("%d  %d\n", space[2].data,space[2].cur);
+	printf("%d  %d\n", space[3].data,space[3].cur);
+	ListDelete(space, 2);
+	printf("%d\n", space[0].cur);
+	return 0;
+}
